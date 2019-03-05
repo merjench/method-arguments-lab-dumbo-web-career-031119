be2c56db81d433797dec8e2905e350d59b1d6f65
Merjen Charyyeva <merjen.chary@gmail.com>
2019-03-05 01:01:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,7 @@ def introduction (name)
 end 
 
 def introduction_with_language (name, language)
-  puts "Hi my name is #{name} and I am learning to program #{language}.\n!" 
+  puts "Hi my name is #{name} and I am learning to program #{language}.\n" 
 end 
 
 introduction_with_language ("Dan", "Ember.js!")
